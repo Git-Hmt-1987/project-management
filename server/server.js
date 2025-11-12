1cd29d4fcc36dcf7a85572fff6dc7f9cb5d35f1f
minh tuan <minhtuan123@gmail.com>
2025-11-12 15:34:41 +0700

    1

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
 import 'dotenv/config'
 import cors from 'cors'
 import { clerkMiddleware } from '@clerk/express'
@@ -10,6 +13,9 @@ app.use(cors())
 app.use(clerkMiddleware())
 
 app.get('/', (req, res) => res.send('Server is live!'))
+
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000
 
